76b79a0779ea2e54d74d022285ab60a799603dda
Pranav Avva <pranav.avva@gmail.com>
2021-07-03 14:10:29 -0400

    Dockerized

--- api/api.py
+++ api/api.py
@@ -2,11 +2,25 @@ from flask import Flask
 from flask_restful import Api, Resource
 import time
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path="/", static_folder="../build")
+
+
+@app.route("/")
+def home():
+    return app.send_static_file("index.html")
+
+
+@app.errorhandler(404)
+def not_found(e):
+    return app.send_static_file("index.html")
+
+
 api = Api(app)
+
 
 class Time(Resource):
     def get(self):
         return {"time": time.time()}
+
 
 api.add_resource(Time, "/api/time")
--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
@@ -16,21 +17,36 @@ function App() {
 
     return (
         <div className="App">
-            <header className="App-header">
+      <header className="App-header">
+        <Router>
+          <div>
+            <Link className="App-link" to="/">Home</Link>
+            &nbsp;|&nbsp;
+            <Link className="App-link" to="/page2">Page2</Link>
+          </div>
+          <Switch>
+            <Route exact path="/">
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>
-                    Edit <code>src/App.tsx</code> and save to reload. Time is: {time}
+                  Edit <code>src/App.js</code> and save to reload.
                 </p>
                 <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
+                  className="App-link"
+                  href="https://reactjs.org"
+                  target="_blank"
+                  rel="noopener noreferrer"
                 >
-                    Learn React
+                  Learn React
                 </a>
-            </header>
-        </div>
+                <p>The current time is {time}.</p>
+            </Route>
+            <Route path="/page2">
+                <p>This is page 2!</p>
+            </Route>
+          </Switch>
+        </Router>
+      </header>
+    </div>
     );
 }
 
